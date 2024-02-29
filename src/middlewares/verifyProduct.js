4a1411dd78656deada801572e229daef0f999055
okabasele <ocekabasele@gmail.com>
2024-02-29 15:44:35 +0100

    :passport_control: Fix SSL configuration and update middleware error handling

--- src/middlewares/verifyProduct.js
+++ src/middlewares/verifyProduct.js
@@ -2,12 +2,10 @@ const Product = require("../models/product.model");
 
 const verifyProduct = async (req, res, next) => {
     const { product_id } = req.body;
-    const product = await Product.findByPk(product_id);
-    if (!product) {
-        return res.status(404).json({ success:false, message: "Product not found" });
-    }
+    Product.findByPk(product_id)
+    .then(() => next())
+    .catch((err) => res.status(404).json({ success: false, message: err.message }));
 
-    next();
 }
 
 module.exports = verifyProduct;
