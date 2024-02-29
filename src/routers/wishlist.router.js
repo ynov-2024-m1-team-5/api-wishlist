fd686affe7d9788a396649d899650fe3280f1ed0
okabasele <ocekabasele@gmail.com>
2024-02-29 15:09:52 +0100

    :passport_control: verify if product exists

--- src/routers/wishlist.router.js
+++ src/routers/wishlist.router.js
@@ -4,8 +4,8 @@ const verifyProduct = require('../middlewares/verifyProduct');
 const verifyUser = require('../middlewares/verifyUser');
 
 router.get('/:customer_id/products', wishlistController.getWishlistByCustomerId);
-router.post('/:customer_id/products/add/:product_id', wishlistController.addProductToWishlist);
-router.put('/:customer_id/products/remove/:product_id', wishlistController.removeProductFromWishlist);
+router.post(verifyProduct,'/:customer_id/products/add/:product_id', wishlistController.addProductToWishlist);
+router.put(verifyProduct,'/:customer_id/products/remove/:product_id', wishlistController.removeProductFromWishlist);
 router.delete('/:customer_id', wishlistController.deleteWishlistByCustomerId);
 
 module.exports = router;
