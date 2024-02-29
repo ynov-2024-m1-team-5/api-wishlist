23cb561ad3c81c93c416bf340b84ce8034695017
okabasele <ocekabasele@gmail.com>
2024-02-29 15:02:25 +0100

    :sparkles: crud wishlist

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const wishlistRouter = require('./wishlist.router');
 const verifyToken = require('../middlewares/verifyToken');
 
-router.use(verifyToken);
-router.use('/wishlist', wishlistRouter);
+// router.use(verifyToken);
+router.use('/wishlists', wishlistRouter);
+
+module.exports = router;
