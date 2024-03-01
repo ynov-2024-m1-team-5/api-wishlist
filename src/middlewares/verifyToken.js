b97c6331b68031231eb13477ebc6db164743ac73
okabasele <ocekabasele@gmail.com>
2024-03-01 11:12:36 +0100

    :passport_control: handling token and authorization

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -1,9 +1,29 @@
-const verifyToken = (req, res, next) => {
-    const token = req.headers.authorization;
-    if (!token) {
-        return res.status(403).json({ success:false, message: "Unauthorized" });
+const jwt = require('jsonwebtoken');
+
+function verifyToken (req, res, next) {
+
+  let token = req.headers.authorization.split(' ')[1];
+  if (!token) {
+    return res.status(403).send({
+      success: false,
+      token: null,
+      message:"Missing token"
+    })
+  }
+ 
+  jwt.verify(token, process.env.JWT_SECRET,{
+    algorithms: [process.env.JWT_ALGORITHM]
+  }, function (error, jwtDecoded) {
+    if (error) {
+      return res.status(401).send({
+        success: false,
+        token: null,
+        message: error.message
+      })
     }
+    req.userToken = jwtDecoded;
     next();
+  })
 }
 
 module.exports = verifyToken;
