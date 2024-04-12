7972826c81fed917c6a66327ae34ceec202c9f8c
okabasele <ocekabasele@gmail.com>
2024-04-12 12:41:16 +0200

    🚑 Fix require statement in product.model.js and wishlist.model.js

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require("sequelize");
-const { sequelize } = require("../database");
+const sequelize = require("../database");
 
 const Product = sequelize.define("Products", {
   id: {
